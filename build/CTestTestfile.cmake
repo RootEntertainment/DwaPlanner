d4a4021a315d044b2a723705efbcc71bd106f2ff
Arles <2377099228@qq.com>
2025-04-27 23:36:52 -0700

    update gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arles/ws_ros/src/dwa_navigation
-# Build directory: /home/arles/ws_ros/src/dwa_navigation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
